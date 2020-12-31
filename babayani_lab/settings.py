455a95018ab226ba3a040c675510fc55e4da5aab
kaanistemi <kaanistemi1@gmail.com>
2020-12-31 18:42:30 +0000

    sending e-mails

--- babayani_lab/settings.py
+++ babayani_lab/settings.py
@@ -197,4 +197,16 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'sk_test_51HAYEAED0ECpcqyZ0tS
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', 'whsec_YClpgtbU4xhSmCji6jc8op47tCgfaOCB')
 DEFAULT_FROM_EMAIL = 'kaanistemi1@gmail.com'
 
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'kaanistemi1@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 
